180ac86351a036d2813e94ef3878b418eff9eb2d
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-07-19 18:58:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
-def speak_to_grandma("phrase")
-  if "phrase"=="I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase=="I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif "phrase" != "phrase".upcase
+  elsif phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif "phrase".upcase=="phrase"
+  elsif phrase.upcase==phrase
     puts "NO,NOT SINCE 1938!"
   else
   end
