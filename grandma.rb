747f97a92774b8662ebf2975bed95cc1378d7b05
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-07-19 18:28:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase.upcase!=phrase
+  if phrase.upcase != phrase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase=phrase
     puts "NO,NOT SINCE 1938!"
